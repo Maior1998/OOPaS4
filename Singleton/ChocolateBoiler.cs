48af19a0549c8e80fe36cca4477c791d6737f694
Sergey Patoka <sergey.pat98@gmail.com>
2020-03-29 18:30:12 +0500

    Add project files.

---
+++ Singleton/ChocolateBoiler.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    /// <summary>
+    /// Представляет собой нагреватель для шоколада.
+    /// </summary>
+    public class ChocolateBoiler
+    {
+        /// <summary>
+        /// Указывает, согрет ли на данный момент шоколад в нагревателе.
+        /// </summary>
+        private static bool isboiled;
+
+        /// <summary>
+        /// Показывает, закипячен ли нагреватель. Это свойство только для чтения.
+        /// </summary>
+        public bool isBoiled => isboiled;
+
+        /// <summary>
+        /// Указывает, пуст ли сейчас нагреватель.
+        /// </summary>
+        private static bool isempty;
+
+        /// <summary>
+        /// Показывает, пуст ли нагреватель. Это свойство только для чтения.
+        /// </summary>
+        public bool isEmpty => isempty;
+
+        /// <summary>
+        /// Сущность нагревателя, которой все пользуются.
+        /// </summary>
+        private static readonly ChocolateBoiler instance;
+        /// <summary>
+        /// Статический конструтор нагревателя. Нужен для заполнения переменной с бойлером (синхронизация потоков) и прочих инициализаций.
+        /// </summary>
+       
+        static ChocolateBoiler()
+        {
+            instance = new ChocolateBoiler();
+        }
+        /// <summary>
+        /// Инициализирует новый пустой экземпляр бойлера.
+        /// </summary>
+        
+        private ChocolateBoiler()
+        {
+            isboiled = false;
+            isempty = true;
+            Console.WriteLine("Boiler Initialized!");
+        }
+
+        /// <summary>
+        /// Заполняет бойлер шоколадом.
+        /// </summary>
+        public void Fill()
+        {
+            if (!isempty) return;
+            isempty = false;
+            isboiled = false;
+            Console.WriteLine("Boiler Filled!!");
+        }
+
+        private static readonly object locker = new object();
+
+        /// <summary>
+        /// Нагревает шоколад в бойлере, если он там есть.
+        /// </summary>
+        public void Boil()
+        {
+
+            lock (locker)
+            {
+                if (isempty || isboiled)
+                {
+                    Console.WriteLine(isempty ? "Boiler is empty!" : "Boiler is already boiled!");
+                    return;
+                }
+                Console.WriteLine($"Start boiling ({Thread.CurrentThread.Name}) . . .");
+                Thread.Sleep(5000);
+                isboiled = true;
+                Console.WriteLine("Boiler Boiled!!");
+            }
+        }
+
+        /// <summary>
+        /// Выливает горячий шоколад из бойлера.
+        /// </summary>
+        public void Drain()
+        {
+            if (isempty || !isboiled) return;
+            isboiled = false;
+            isempty = true;
+            Console.WriteLine("Boiler Drained!!");
+        }
+
+        /// <summary>
+        /// Получает сущность данного бойлера.
+        /// </summary>
+        /// <returns>Бойлер. Просто бойлер.</returns>
+        public static ChocolateBoiler GetBoiler()
+        {
+            return instance;
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChocolateBoiler test = ChocolateBoiler.GetBoiler();
+            test.Fill();
+            Task.Run(()=>test.Boil());
+            Thread.Sleep(2000);
+            test.Boil();
+            Console.WriteLine();
+        }
+    }
+}
